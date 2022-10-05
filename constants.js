ba293bbade0e434ae2eac0b56b90452213d238e2
Serhii Sovhut <serhii.sovhut@deepinspire.com>
2022-10-05 20:54:25 +0300

    Fixed prints arrays in exception message.

--- constants.js
+++ constants.js
@@ -1,19 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Message = void 0;
+const print = (value) => {
+    if (typeof value === 'string')
+        return value;
+    if (value === null)
+        return 'null';
+    if (typeof value === 'undefined')
+        return 'undefined';
+    if (typeof value === 'object' && Array.isArray(value))
+        return `[${value.join(',')}]`;
+    if (typeof value === 'object')
+        return JSON.stringify(value);
+    return value.toString();
+};
 exports.Message = {
     NotArray: (scheme, key, value) => {
         var _a, _b, _c;
-        return `(${key}): Invalid array value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'. Allowed values: [${(_a = scheme.items) === null || _a === void 0 ? void 0 : _a.type},${(_b = scheme.items) === null || _b === void 0 ? void 0 : _b.type},${(_c = scheme.items) === null || _c === void 0 ? void 0 : _c.type}...].`;
+        return `(${key}): Invalid array value: '${print(value)}'. Allowed values: [${(_a = scheme.items) === null || _a === void 0 ? void 0 : _a.type},${(_b = scheme.items) === null || _b === void 0 ? void 0 : _b.type},${(_c = scheme.items) === null || _c === void 0 ? void 0 : _c.type}...].`;
     },
-    NotInteger: (scheme, key, value) => `(${key}): Invalid integer value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotNumber: (scheme, key, value) => `(${key}): Invalid number value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotBoolean: (scheme, key, value) => `(${key}): Invalid boolean value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotTimestamp: (scheme, key, value) => `(${key}): Invalid date-time value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotObject: (scheme, key, value) => `(${key}): Invalid object value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotString: (scheme, key, value) => `(${key}): Invalid string value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    NotInRange: (scheme, key, value) => `(${key}): Invalid string length: '${value === null || value === void 0 ? void 0 : value.toLocaleString().length}'. Allowed range: ${scheme.minLength}-${scheme.maxLength}`,
-    NotInEnum: (scheme, key, value) => { var _a; return `(${key}): Invalid enum value: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'. Allowed values: [${(_a = scheme.enum) === null || _a === void 0 ? void 0 : _a.toLocaleString()}].`; },
-    Required: (scheme, key, value) => `(${key}): Is required. Received: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
-    Invalid: (scheme, key, value) => `(${key}): Failed additional validation. Received: '${value === null || value === void 0 ? void 0 : value.toLocaleString()}'.`,
+    NotInteger: (scheme, key, value) => `(${key}): Invalid integer value: '${print(value)}'.`,
+    NotNumber: (scheme, key, value) => `(${key}): Invalid number value: '${print(value)}'.`,
+    NotBoolean: (scheme, key, value) => `(${key}): Invalid boolean value: '${print(value)}'.`,
+    NotTimestamp: (scheme, key, value) => `(${key}): Invalid date-time value: '${print(value)}'.`,
+    NotObject: (scheme, key, value) => `(${key}): Invalid object value: '${print(value)}'.`,
+    NotString: (scheme, key, value) => `(${key}): Invalid string value: '${print(value)}'.`,
+    NotInRange: (scheme, key, value) => `(${key}): Invalid string length: '${print(value).length}'. Allowed range: ${scheme.minLength}-${scheme.maxLength}`,
+    NotInEnum: (scheme, key, value) => {
+        var _a;
+        return `(${key}): Invalid enum value: '${print(value)}'. Allowed values: [${(_a = scheme.enum) === null || _a === void 0 ? void 0 : _a.toLocaleString()}].`;
+    },
+    Required: (scheme, key, value) => `(${key}): Is required. Received: '${print(value)}'.`,
+    Invalid: (scheme, key, value) => `(${key}): Failed additional validation. Received: '${print(value)}'.`,
 };
--- constants.ts
+++ constants.ts
@@ -1,30 +1,43 @@
 import { Field } from './types';
 
+const print = (value: any) => {
+  if (typeof value === 'string') return value;
+  if (value === null) return 'null';
+  if (typeof value === 'undefined') return 'undefined';
+  if (typeof value === 'object' && Array.isArray(value))
+    return `[${value.join(',')}]`;
+  if (typeof value === 'object') return JSON.stringify(value);
+
+  return value.toString();
+};
+
 export const Message = {
   NotArray: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid array value: '${value?.toLocaleString()}'. Allowed values: [${
+    `(${key}): Invalid array value: '${print(value)}'. Allowed values: [${
       scheme.items?.type
     },${scheme.items?.type},${scheme.items?.type}...].`,
   NotInteger: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid integer value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid integer value: '${print(value)}'.`,
   NotNumber: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid number value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid number value: '${print(value)}'.`,
   NotBoolean: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid boolean value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid boolean value: '${print(value)}'.`,
   NotTimestamp: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid date-time value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid date-time value: '${print(value)}'.`,
   NotObject: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid object value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid object value: '${print(value)}'.`,
   NotString: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid string value: '${value?.toLocaleString()}'.`,
+    `(${key}): Invalid string value: '${print(value)}'.`,
   NotInRange: (scheme: Field, key: string, value: any) =>
     `(${key}): Invalid string length: '${
-      value?.toLocaleString().length
+      print(value).length
     }'. Allowed range: ${scheme.minLength}-${scheme.maxLength}`,
   NotInEnum: (scheme: Field, key: string, value: any) =>
-    `(${key}): Invalid enum value: '${value?.toLocaleString()}'. Allowed values: [${scheme.enum?.toLocaleString()}].`,
+    `(${key}): Invalid enum value: '${print(
+      value,
+    )}'. Allowed values: [${scheme.enum?.toLocaleString()}].`,
   Required: (scheme: Field, key: string, value: any) =>
-    `(${key}): Is required. Received: '${value?.toLocaleString()}'.`,
+    `(${key}): Is required. Received: '${print(value)}'.`,
   Invalid: (scheme: Field, key: string, value: any) =>
-    `(${key}): Failed additional validation. Received: '${value?.toLocaleString()}'.`,
+    `(${key}): Failed additional validation. Received: '${print(value)}'.`,
 };
